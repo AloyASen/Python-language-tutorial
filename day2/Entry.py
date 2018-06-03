882fa3b3ab1cf48d3e912dd2aa72965a12bd7973
Aloy Aditya Sen <aloysen951@gmail.com>
2018-06-03 20:02:11 +0530

    day 2 changes are added

---
+++ day2/Entry.py
@@ -0,0 +1,7 @@
+import Tuna
+#generate random numbers 
+import random
+#adds funcionality that is not added to the core framework of the language
+x= random.randrange(1,1000)
+print(x)
+Tuna.fish()
---
+++ day2/modify.md
@@ -0,0 +1,4 @@
+# wht the fuck is contained here
+
+
+this is  an example of implementimg modules in python --- they are somewhat similar to "packages"in java
