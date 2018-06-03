9defb56de2f2e4b18e3cc7a1570f3c262aff5c1d
Aloy Aditya Sen <aloysen951@gmail.com>
2018-06-03 20:12:02 +0530

    fresh into design patterns
    these files are not required for a fresh start indesign patterns in python

--- day2/Entry.py
+++
@@ -1,7 +0,0 @@
-import Tuna
-#generate random numbers 
-import random
-#adds funcionality that is not added to the core framework of the language
-x= random.randrange(1,1000)
-print(x)
-Tuna.fish()
--- day2/modify.md
+++
@@ -1,4 +0,0 @@
-# wht the fuck is contained here
-
-
-this is  an example of implementimg modules in python --- they are somewhat similar to "packages"in java
